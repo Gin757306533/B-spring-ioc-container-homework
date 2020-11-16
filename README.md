399571e28472e99fd4bae3fab3fa3b5e6b5b862d
Gin757306533 <757306533@qq.com>
2020-11-16 19:34:16 +0800

    SP[xj-1]: second answer，second method

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ GreetingService 现在必须为 prototype scope，如何保证每次 GreetingCon
 至少2种解决方法且
 GreetingController 仍为 singleton scope。
 每种方案分别提交到单独的分支上，这些分支都需从 p2-starts-from-here 分出来，依次命名为 p2s1、p2s2即可。
+
+方案二：利用@Lookup方法，spring会覆盖带有@Lookup注解的方法
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class GreetingService {
+    public GreetingService(){
+        System.out.println("this will create a new GreetingService");
+    }
 
     String sayHi() {
         return "hello world";
